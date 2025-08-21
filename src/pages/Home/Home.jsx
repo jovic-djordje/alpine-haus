4e2171abff9a083fc032bd7090666b7e39f333fe
jovic-djordje <jovicdjordje21@gmail.com>
2025-08-21 12:32:21 +0200

    working on hero section in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+import "./home.style.css";
+
 const Home = () => {
-  return <h1>caoo</h1>;
+  return (
+    <section className="hero-section">
+      <div className="hero-section-holder">
+        <h1 className="hero-title">Experience true alpine hospitality</h1>
+        <p className="hero-text">
+          Indluge in authentic <span>austrian-german</span> cuisine that warms
+          the heart and soul. Join us in a cozy, rustic setting that feels like
+          home.
+        </p>
+        <div className="hero-btn-holder">
+          <Link to="/menu" className="hero-link">
+            <button className=" hero-btn hero-menu-btn">View Menu</button>
+          </Link>
+          <Link className="hero-link">
+            <button className="hero-btn">Reserve</button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
