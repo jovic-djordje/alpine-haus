469334eae957bc01131177be83a91c4a87fa401f
jovic-djordje <jovicdjordje21@gmail.com>
2025-09-07 23:32:55 +0200

    working on special page

--- src/pages/Specials/Specials.jsx
+++ src/pages/Specials/Specials.jsx
@@ -1,5 +1,27 @@
+import { SpecialImg } from "../../assets/images";
+import { Link } from "react-router-dom";
+import "./specials.style.css";
+
 const Specials = () => {
-  return <h1>Kontakt</h1>;
+  return (
+    <main>
+      <section className="specials">
+        <div className="specials-holder sections-holder">
+          <div className="specials-header-holder">
+            <h1>4</h1>
+            <SpecialImg className="special-img rotating-plate" />
+            <h1>4</h1>
+          </div>
+          <h2 className="special-text">
+            Our chef is cooking up something special
+          </h2>
+          <Link to="/#top" className="special-link">
+            <button className="special-btn hero-btn">Go Home</button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Specials;
